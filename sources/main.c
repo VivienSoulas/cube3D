251bd81d097cf17976645a4a422f30d9529da013
Vivien Soulas <vsoulas@f1r5s6.codam.nl>
2025-07-24 16:02:58 +0200

    norm

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: vsoulas <vsoulas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 14:17:15 by vsoulas           #+#    #+#             */
-/*   Updated: 2025/07/24 15:56:20 by vsoulas          ###   ########.fr       */
+/*   Updated: 2025/07/24 16:02:05 by vsoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int	main(int ac, char **av)
 		return (ft_error(1), 1);
 	fd = open(av[1], O_RDONLY);
 	if (fd == -1)
-		return(ft_error(2), 1);
+		return (ft_error(2), 1);
 	if (ft_map_parsing(fd) == 1)
-		return(ft_error(3), 1);
+		return (ft_error(3), 1);
 
 	printf("My cube3D starts\n");
 	return (close(fd), 0);
