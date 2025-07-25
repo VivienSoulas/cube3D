f4182df7dd21ce0193a264fcdbb2e66d795e0e06
Vivien Soulas <vsoulas@f1r9s12.codam.nl>
2025-07-25 17:23:46 +0200

    parsing

--- sources/map_parsing.c
+++ sources/map_parsing.c
@@ -6,7 +6,7 @@
 /*   By: vsoulas <vsoulas@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 16:02:29 by vsoulas           #+#    #+#             */
-/*   Updated: 2025/07/25 17:20:54 by vsoulas          ###   ########.fr       */
+/*   Updated: 2025/07/25 17:23:20 by vsoulas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,12 @@ int	ft_map_parsing(int fd)
 {
 	char	*line;
 
-	while ((line = get_next_line(fd)) != NULL)
+	line = get_next_line(fd);
+	while (line != NULL)
 	{
 		printf("%s", line);
 		free(line);
+		line = get_next_line(fd);
 	}
 	return (0);
 }
